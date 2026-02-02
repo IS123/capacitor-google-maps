da3ca1abdba6735f0e938671adab2c374edbefc1
Volodymyr Holodivskyi <volodymyrholodivskyi@gmail.com>
2026-02-02 16:03:22 +0200

    android: fix dragging

--- plugin/android/src/main/java/com/capacitorjs/plugins/googlemaps/CapacitorGoogleMap.kt
+++ plugin/android/src/main/java/com/capacitorjs/plugins/googlemaps/CapacitorGoogleMap.kt
@@ -248,20 +248,10 @@ class CapacitorGoogleMap(
 						val currentMIds = mutableSetOf<String>()
 						val existingMIdsSnapshot = mIds.keys.toSet()
 
-						val markersToAdd = newMarkers.mapNotNull { marker ->
+						val markersToAdd = newMarkers.map { marker ->
 							currentMIds += marker.mId
-							val existingId = mIds[marker.mId]
-							if (existingId != null) {
-								withContext(Dispatchers.Main) {
-									val existingMarker = markers[existingId]
-
-									existingMarker?.googleMapMarker?.position = marker.position
-								}
-								return@mapNotNull null
-							} else {
-								marker.markerOptions = buildMarker(marker)
-								marker
-							}
+							marker.markerOptions = buildMarker(marker)
+							marker
 						}
 
 						withContext(Dispatchers.Main) {
